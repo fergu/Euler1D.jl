0bff87d67042db12faf0709d3e3c96cefbe465c9
Kevin <9022843+fergu@users.noreply.github.com>
2026-01-01 18:51:04 -0700

    Updated Sod Shock Tube example

--- examples/SodsShockTube.jl
+++ examples/SodsShockTube.jl
@@ -31,7 +31,6 @@ function P( x::Float64 )
 end
 
 simulation_parameters = DefaultSimulationParameters() # Get a default set of parameters
-simulation_parameters["end_time"] = 0.1 # Set the end time to 0.1 seconds
 simulation_parameters["init_density_function"] = ρ # Set the function used to initialize density as the "ρ" function, above
 simulation_parameters["init_velocity_function"] = u # Set the function used to initialize velocity as the "u" function, above
 simulation_parameters["init_pressure_function"] = P # Set the function used to initialize pressure as the "P" function, above
@@ -40,7 +39,7 @@ simulation_parameters["artificial_conductivity_coefficient"] = 1.0e-2 # Set the 
 simulation_parameters["artificial_viscosity_coefficient"] = 1.0e0 # Set the artificial viscosity coefficient. This value is the same as the default value, but is included here for demonstration
 simulation_parameters["number_of_zones"] = 6000 # Set the number of zones in the problem
 init_state = InitializeSimulation( simulation_parameters ) # Initialize the simulation. This sets up the various arrays and such needed for the simulation and returns them in the `init_state` variable
-final_state = AdvanceToTime( init_state, init_state.t₁; exact=true ) # Run the simulation to the final time
+final_state = AdvanceToTime( init_state, 0.1; exact=true ) # Run the simulation to the final time
 
 # At this point you can explore the final problem state
 println( final_state )
